2cdfdcc95b97f4787a56cdba5b37c102a7633fec
muradwahid <muradwahid334@gmail.com>
2022-11-19 14:49:49 +0600

    privet route added

--- src/Layout/Main/Main.js
+++ src/Layout/Main/Main.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Shared/Footer/Footer';
 import Navbar from '../../Shared/Navbar/Navbar';
 
 const Main = () => {
     return (
         <>
             <Navbar />
-         <Outlet/>   
+            <Outlet />  
+            <Footer/>
         </>
     );
 };
